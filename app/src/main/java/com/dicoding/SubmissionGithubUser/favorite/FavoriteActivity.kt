7daa6f9ad99ebdb821829b8723e3b525ca8f5d56
izzudin17 <izzudinadian@gmail.com>
2023-11-02 17:00:55 +0700

    first init github

---
+++ app/src/main/java/com/dicoding/SubmissionGithubUser/favorite/FavoriteActivity.kt
@@ -0,0 +1,58 @@
+package com.dicoding.SubmissionGithubUser.favorite
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.SubmissionGithubUser.UserAdapter
+import com.dicoding.SubmissionGithubUser.data.local.DbModule
+import com.dicoding.SubmissionGithubUser.databinding.ActivityFavoriteBinding
+import com.dicoding.SubmissionGithubUser.detail.DetailActivity
+
+class FavoriteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFavoriteBinding
+    private val adapter by lazy {
+        UserAdapter { user ->
+            Intent(this, DetailActivity::class.java).apply {
+                putExtra("item", user)
+                startActivity(this)
+            }
+        }
+    }
+
+    private val viewModel by viewModels<FavoriteViewModel> {
+        FavoriteViewModel.Factory(DbModule(this))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFavoriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.rvFavorite.layoutManager = LinearLayoutManager(this)
+        binding.rvFavorite.adapter = adapter
+
+        viewModel.getUserFavorite().observe(this) {
+            adapter.setData(it)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getUserFavorite().observe(this) {
+            adapter.setData(it)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
